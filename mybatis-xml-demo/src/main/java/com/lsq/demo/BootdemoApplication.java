6691940a45f00b22797ce7f9f1581f429b25ab11
liushaoqing <liushaoqing@eversec.cn>
2020-09-24 13:49:00 +0800

    mybatis xml 配置

--- mybatis-xml-demo/src/main/java/com/lsq/demo/BootdemoApplication.java
+++ mybatis-xml-demo/src/main/java/com/lsq/demo/BootdemoApplication.java
@@ -1,11 +1,13 @@
 package com.lsq.demo;
 
 import com.lsq.demo.mapper.CityMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lsq.demo.mapper")
 public class BootdemoApplication {
 
 	public static void main(String[] args) {
--- mybatis-xml-demo/src/main/java/com/lsq/demo/mapper/CityMapper.java
+++ mybatis-xml-demo/src/main/java/com/lsq/demo/mapper/CityMapper.java
@@ -1,14 +1,8 @@
 package com.lsq.demo.mapper;
 
 import com.lsq.demo.model.City;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface CityMapper {
 
-    @Select("SELECT * FROM CITY WHERE name = #{name}")
-    City findByName(@Param("name") String state);
-
+    City findByName(String name);
 }
